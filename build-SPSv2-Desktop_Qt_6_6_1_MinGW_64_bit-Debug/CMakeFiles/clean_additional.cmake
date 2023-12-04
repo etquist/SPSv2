f4fd3ecf822eb7f0388b7a4516f170c25d977385
etquist <150300772+etquist@users.noreply.github.com>
2023-12-04 11:07:03 -0500

    QT Project
    New project built on Windows (not linux like before)

---
+++ build-SPSv2-Desktop_Qt_6_6_1_MinGW_64_bit-Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\SPSv2_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\SPSv2_autogen.dir\\ParseCache.txt"
+  "SPSv2_autogen"
+  )
+endif()
